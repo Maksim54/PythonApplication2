6ded518125458d180da6c6728524b42ffe26d825
Лагуновский Максим <mlagunovski@gmail.com>
2021-01-12 16:23:12 +0200

    Добавьте файлы проекта.

---
+++ PythonApplication2/PythonApplication2.py
@@ -0,0 +1,23 @@
+print("Чтобы завершить работу программы"
+      "\nвведите ноль")
+while True:
+    s = input("Выберите способ считывания (+,-,*,/): ")
+    if s == '0':
+        break
+    if s in ('+', '-', '*', '/'):
+        x = float(input("x="))
+        y = float(input("y="))
+        if s == '+':
+            print("%.2f" % (x+y))
+        elif s == '-':
+            print("%.2f" % (x-y))
+        elif s == '*':
+            print("%.2f" % (x*y))
+        elif s == '/':
+            if y != 0:
+                print("%.2f" % (x/y))
+            else:
+                print("Деление на ноль!")
+    else:
+        print("Неверный знак операции!")
+
